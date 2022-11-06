775da5ceef73c7a046ebc98ecb714311661bc344
venomsonly <106017107+venomsonly@users.noreply.github.com>
2022-11-06 20:46:48 +0530

    ok

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,6 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <><h1>this is cf2</h1></>
+    <><h1>this is cf2 {process.env.NEXT_PUBLIC_HOSTNAME}</h1></>
   )
 }
